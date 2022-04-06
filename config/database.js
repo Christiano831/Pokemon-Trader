0aadd7f0c6bbff47eb656151622c97072d1dcf3b
Christiano831 <cespinola@live.com>
2022-04-05 21:01:01 -0700

    fix server issue

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/pokemon', {
+mongoose.connect('mongodb://localhost/pokemons', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
